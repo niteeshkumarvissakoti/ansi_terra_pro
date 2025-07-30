7d4bfbfa53b9162ec64f703f042e23f6351853c6
niteeshkumarvissakoti <klu2100032045@gmail.com>
2025-07-30 11:43:09 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,7 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-020cba7c55df1f615"
   instance_type = "t3.micro"
+  key_name      = "EC2_PRIVATE_KEY"
   tags = {
     Name = "terra_ansi_pro" 
     }
