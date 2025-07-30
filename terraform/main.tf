c4065076d9fbaacf8671da390537e1e25c0eb15b
niteeshkumarvissakoti <klu2100032045@gmail.com>
2025-07-30 01:16:34 -0400

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,17 @@
+
+
+provider "aws" {
+  region="us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-020cba7c55df1f615"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "terra_ansi_pro" 
+    }
+
+    provisioner "local-exec" {
+    command = "echo ${self.public_ip} > ip.txt"
+  }
+}
