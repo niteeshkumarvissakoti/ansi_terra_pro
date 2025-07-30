07886d50a4c3b41f2854d4342cd4eda44d2f9ef1
niteeshkumarvissakoti <klu2100032045@gmail.com>
2025-07-30 17:51:52 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,6 +18,29 @@ resource "aws_instance" "web" {
     working_dir = "${path.module}"
   }
 }
-resource "aws_ec2_instance_connect_endpoint" "web" {
-  subnet_id = aws_subnet.web.id
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+output "ec2_instance_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.myec2.public_ip
 }
